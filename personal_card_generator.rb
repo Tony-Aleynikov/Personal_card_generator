473cc0c45e0be29e1d7a474cca2213fcbec15d45
Tony <tonyaleynikov@gmail.com>
2021-11-10 10:14:50 +0300

    Добавлен метод get_description

--- personal_card_generator.rb
+++ personal_card_generator.rb
@@ -2,5 +2,5 @@ puts "Вы должны ввести данные для заполнения в
 
 data = []
 
-data << get_name << get_discription << get_phone << get_email
+data << get_name << get_description << get_phone << get_email
 
